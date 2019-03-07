c50496a97fa6faca1c3e4ccbd739273173d3fb25
Ovidio R. Calvet <calveto@gmail.com>
2019-03-07 16:08:40 -0500

    Add producer

--- producer.go
+++ producer.go
@@ -2,11 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/streadway/amqp"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
 func main() {
-	config := amqp.Config{}
-	fmt.Print(config)
+	conn, err := amqp.Dial("amqp://producer:password.producer@137.117.65.53:5672/appsuite")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	fmt.Println("DONE")
 }
